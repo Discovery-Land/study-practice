14c55dd9baa03a85a76778cc67905a4b7f3cf0ff
김지환 <rlawl4240@gmail.com>
2026-02-25 06:02:37 +0900

    [FEAT] 모든 레코드 조회하기

---
+++ 00_sql-analytics-practice/programmers/2026/02/Lv1_59034.sql
@@ -0,0 +1,8 @@
+-- Site   : Programmers
+-- Title  : 모든 레코드 조회하기 (LEVEL 1)
+-- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/59034
+-- Date   : 2026-02-25
+
+SELECT *
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
