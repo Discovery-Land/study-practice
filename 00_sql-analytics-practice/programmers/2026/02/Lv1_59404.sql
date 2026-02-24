1ea00336742cd697c34cb2461bc0be2af9a81bfb
김지환 <rlawl4240@gmail.com>
2026-02-25 06:05:50 +0900

    [FEAT] 여러 기준으로 정렬하기

---
+++ 00_sql-analytics-practice/programmers/2026/02/Lv1_59404.sql
@@ -0,0 +1,11 @@
+-- Site   : Programmers
+-- Title  : 여러 기준으로 정렬하기 (LEVEL 1)
+-- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/59404
+-- Date   : 2026-02-25
+
+SELECT 
+    ANIMAL_ID,
+    NAME,
+    DATETIME
+FROM ANIMAL_INS
+ORDER BY NAME, DATETIME DESC;
