91253a818c40ac56aee5a3ad87d7b6febbaf40ec
김지환 <rlawl4240@gmail.com>
2026-02-26 06:00:54 +0900

    [FEAT] 언어별 개발자 분류하기

---
+++ 00_sql-analytics-practice/programmers/2026/02/Lv4_ 276036.sql
@@ -0,0 +1,18 @@
+-- Site   : Programmers
+-- Title  : 언어별 개발자 분류하기 (LEVEL 4)
+-- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/276036
+-- Date   : 2026-02-26
+
+-- A : Front End 스킬과 Python 스킬을 함께 가지고 있는 개발자
+--  # CATEGORY가 Front End인 2진코드
+-- B : C# 스킬을 가진 개발자
+--  # CODE 1024
+-- # C : 그 외의 Front End 개발자
+
+WITH skill as (
+    SELECT
+        
+    FROM SKILLCODES
+)
+
+-- 2진코드 문제 재학습 필요
