1055b71af436e731191be36b223aace05e96bc2c
김지환 <rlawl4240@gmail.com>
2026-02-27 06:57:11 +0900

    [FIX] 동명 동물 수 찾기

--- 00_sql-analytics-practice/programmers/2026/02/Lv2_59041.sql
+++ 00_sql-analytics-practice/programmers/2026/02/Lv2_59041.sql
@@ -1,10 +1,16 @@
 -- Site   : Programmers
 -- Title  : 동명 동물 수 찾기 (LEVEL 2)
 -- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/59041
--- Date   : 2026-02-13
+-- Date   : 2026-02-27
 
-SELECT NAME, COUNT(*) COUNT
+SELECT 
+    NAME,
+    COUNT(*) COUNT
 FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
 GROUP BY NAME
 HAVING COUNT >= 2
 ORDER BY NAME;
+
+
+-- 이름 없는 동물 제외
