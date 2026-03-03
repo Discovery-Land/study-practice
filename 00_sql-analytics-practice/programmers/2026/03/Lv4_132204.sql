854c6c0641946bf6b2245f999b355dc91c885cf9
김지환 <rlawl4240@gmail.com>
2026-03-04 07:26:42 +0900

    Lv4_132204.sql 만들기

---
+++ 00_sql-analytics-practice/programmers/2026/03/Lv4_132204.sql
@@ -0,0 +1,9 @@
+-- Site   : Programmers
+-- Title  : 취소되지 않은 진료 예약 조회하기 (LEVEL 4)
+-- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/131124
+-- Date   : 2026-03-04
+
+SELECT 
+FROM 
+
+-- 모바일로 쉽지 않음
