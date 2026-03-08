8c1bc455aec82b242217e71cc2749efb250a24c9
김지환 <rlawl4240@gmail.com>
2026-03-09 07:58:47 +0900

    [FEAT] 보호소에서 중성화한 동물

---
+++ 00_sql-analytics-practice/programmers/2026/02/Lv4_284528 copy.sql
@@ -0,0 +1,14 @@
+-- Site   : Programmers
+-- Title  : 보호소에서 중성화한 동물 (LEVEL 4)
+-- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/59045
+-- Date   : 2026-03-09
+
+SELECT 
+    AI.ANIMAL_ID,
+    AI.ANIMAL_TYPE,
+    AI.NAME
+FROM ANIMAL_INS AI
+JOIN ANIMAL_OUTS AO ON AI.ANIMAL_ID = AO.ANIMAL_ID 
+WHERE AI.SEX_UPON_INTAKE LIKE ('Intact%')
+    AND AO.SEX_UPON_OUTCOME NOT LIKE ('Intact%')
+ORDER BY ANIMAL_ID ASC;
