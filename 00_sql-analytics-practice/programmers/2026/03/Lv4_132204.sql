0bff124626b69607406576aaa7303d638619fb3e
김지환 <rlawl4240@gmail.com>
2026-03-12 07:48:51 +0900

    Lv4_132204.sql 업데이트

--- 00_sql-analytics-practice/programmers/2026/03/Lv4_132204.sql
+++ 00_sql-analytics-practice/programmers/2026/03/Lv4_132204.sql
@@ -1,17 +1,17 @@
 -- Site   : Programmers
 -- Title  : 취소되지 않은 진료 예약 조회하기 (LEVEL 4)
 -- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/131124
--- Date   : 2026-03-04
+-- Date   : 2026-03-12
 
-SELECT 
-FROM 
-
--- 모바일로 쉽지 않음
-
---
 SELECT
+A.APNT_NO,
+P.PT_NAME,
+P.PT_NO,
+A.MCDP_CD,
+DR_NAME,
+A.APNT_YMD
 FROM
+APPOINTMENT A
+JOIN DoCTOR D ON A.MDDR_ID = D.DR_ID
 
--- ㅊㅜㅣㅅㅗㄷㅗㅣㅈㅣ ㅇㅏㄴㅎㅇㅡㄴ ㅋㅜㅓㄹㅣㅎㅏㄴㅡㄴ ㅂㅏㅇㅂㅓㅂ 
-
--- ㅁㅗㅂㅏㄹㅣㄹ ㅌㅏㅇㅣㅍㅣㅇ ㅇㅗㄹㅠ
+-- 시간초과
